9ccf18c8901935bfa31a508c900a8e88455e0c2e
Clement Habinshuti <hello@habbes.xyz>
2017-04-26 11:39:04 +0300

    Alternating players

--- src/Board.js
+++ src/Board.js
@@ -5,14 +5,22 @@ class Board extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick (i) {
     const squares = [...this.state.squares];
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.getNextPlayer();
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  getNextPlayer () {
+    return this.state.xIsNext? 'X' : 'O';
   }
 
   renderSquare (i) {
@@ -23,7 +31,7 @@ class Board extends React.Component {
     );
   }
   render () {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.getNextPlayer()}`;
     return (
       <div>
         <div className="status">{status}</div>
